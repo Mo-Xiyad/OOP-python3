23a1f6118c7037cd9011e6683fb23dc8300bcee7
Mo-Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2021-04-13 00:41:28 +0200

    Update and rename oop.py to class_attr.py

---
+++ class_attr.py
@@ -0,0 +1,24 @@
+
+###################
+# Attributes
+###################
+
+# The syntax for creating an attribute is:
+#
+#     self.attribute = something
+#
+# There is a special method called:
+#
+#     __init__()
+#
+# This method is used to initialize the attributes of an object. For example:
+
+
+class Dog():
+    def __init__(self,breed,name):
+        self.breed = breed
+        slef.name = name
+
+lotta = Dog(breed='Labrador Retriever', name="Milly")
+
+zee = Dog('Huskie', 'Morris')
